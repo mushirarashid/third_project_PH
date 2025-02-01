5df1b38ba4b32f81b70189cf192fde806fec6420
mushirarashid <mushirarashid2003@gmail.com>
2025-02-01 23:15:11 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+    // tailwind.config = {
+    //   theme: {
+    //     fontFamily:{
+    //       test: "Source Serif 4"
+    //     },
+    //     extend: {
+    //       colors: {
+    //         clifford: '#da373d',
+    //       }
+    //     }
+    //   }
+    // }
